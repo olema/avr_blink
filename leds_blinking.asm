4f8ebe9a184bddb4635918f8476d305f08c64a16
Oleg Matushkin <matushkin.oleg@gmail.com>
2025-01-04 12:17:44 +0300

    first

---
+++ leds_blinking.asm
@@ -0,0 +1,4 @@
+; Светодиодная мигалка на микроконтроллере  Atmega8L-8PU
+; https://ph0en1x.net/79-avr-asm-first-program-for-microcontroller.html
+
+.INCLUDEPATH "/usr/share/avra/" ; путь для подгрузки .inc файлов
