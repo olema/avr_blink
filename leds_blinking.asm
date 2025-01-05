92222080125893b2f770f752eaa4068dfbceaae8
Oleg Matushkin <matushkin.oleg@gmail.com>
2025-01-05 11:15:38 +0300

    second

--- leds_blinking.asm
+++ leds_blinking.asm
@@ -2,3 +2,23 @@
 ; https://ph0en1x.net/79-avr-asm-first-program-for-microcontroller.html
 
 .INCLUDEPATH "/usr/share/avra/" ; путь для подгрузки .inc файлов
+.INCLUDE "m8def.inc"
+.LIST				; вклюение генерации листинга
+
+.CSEG
+.ORG 0x0000
+
+; -- инициализация стека --
+LDI R16, Low(RAMEND)
+OUT SPL, R16
+LDI R16, High(RAMEND)
+OUT SPH, R16
+
+.equ Delay = 5
+
+; -- устанавливаем каналы PD0 и PD1 порта PORTD (PD) на вывод --
+LDI R16, 0b00000011
+OUT DDRD, R16
+
+Start:
+	SBI PORTD
