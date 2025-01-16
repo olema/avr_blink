b9429a13375d118c7d9c1f8c854f37f11ae3c651
ollo <matushkin.oleg@gmail.com>
2025-01-16 11:10:59 +0300

    some edit in button press

--- count_button_press.asm
+++ count_button_press.asm
@@ -16,6 +16,25 @@
 .org 0x0
 
 ; вектора прерываний
+	rjmp RESET ; Reset Handler
+	reti; rjmp EXT_INT0 ; IRQ0 Handler
+	reti; rjmp EXT_INT1 ; IRQ1 Handler
+	reti; rjmp TIM2_COMP ; Timer2 Compare Handler
+	reti; rjmp TIM2_OVF ; Timer2 Overflow Handler
+	reti; rjmp TIM1_CAPT ; Timer1 Capture Handler
+	reti; rjmp TIM1_COMPA ; Timer1 CompareA Handler
+	reti; rjmp TIM1_COMPB ; Timer1 CompareB Handler
+	reti; rjmp TIM1_OVF ; Timer1 Overflow Handler
+	reti; rjmp TIM0_OVF ; Timer0 Overflow Handler
+	reti; rjmp SPI_STC ; SPI Transfer Complete Handler
+	reti; rjmp USART_RXC ; USART RX Complete Handler
+	reti; rjmp USART_UDRE ; UDR Empty Handler
+	reti; rjmp USART_TXC ; USART TX Complete Handler
+	reti; rjmp ADC ; ADC Conversion Complete Handler
+	reti; rjmp EE_RDY ; EEPROM Ready Handler
+	reti; rjmp ANA_COMP ; Analog Comparator Handler
+	reti; rjmp TWSI ; Two-wire Serial Interface Handler
+	reti; rjmp SPM_RDY ; Store Program Memory Ready Handler
 
 ; инициализация стека
 RESET:
@@ -23,3 +42,8 @@ RESET:
 	out SPL, temp
 	ldi temp, High(RAMEND)
 	out SPH, temp
+; программа
+	ldi temp, 0b00000100	; для второго разряда порта D
+	out PORTD, temp		; вкл подтягивающего резистора
+	ldi temp, 0b11111111	; 
+	out DDRB, temp		; порт B все контакты на выход
